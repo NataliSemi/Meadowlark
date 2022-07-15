a19128da38896a847ec6995d68de9326e015f860
NataliSemi <nata654s@yahoo.com>
2022-07-15 16:46:55 +0300

    addings

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 
-var app = express()
+var app = express();
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
 
@@ -12,29 +12,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-// set 'showTests' context property if the querystring contains test=1
-app.use(function(req, res, next){
-	res.locals.showTests = app.get('env') !== 'production' &&
-		req.query.test === '1';
-	next();
-});
-
 app.get('/', function(req,res){
   res.render('home');
-})
+});
 
 app.get('/about', function(req,res){
-  res.render('about', { fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js' });
-})
-
-app.get('/tours/hood-river', function(req, res){
-	res.render('tours/hood-river');
-});
-app.get('/tours/oregon-coast', function(req, res){
-	res.render('tours/oregon-coast');
-});
-app.get('/tours/request-group-rate', function(req, res){
-	res.render('tours/request-group-rate');
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 
